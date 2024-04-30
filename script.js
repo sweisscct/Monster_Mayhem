0012f699e474cb2dd1bd471e7b8029750ce017be
Sam Weiss <sweiss@cct.ie>
2024-04-30 16:24:38 +0100

    Initial Prompt Code - ChatGPT 3.5
    Congratulations! You’ve been hired to develop a new web-based board game called “Monster Mayhem”. This game is played on a 10x10 grid of squares, with each side of the grid belonging to one player.
    Game Mechanics:
    Each round, the player with the fewest monsters on the grid gets a turn first. If there is a tie, then it is randomly decided from among those with the least monsters.
    On a players turn, they may play either a vampire, a werewolf or a ghost anywhere on their edge of the grid. They may not move that monster this turn. They may also move any other monsters that they have. A monster can move any number of squares horizontally or vertically, or up to two squares diagonally. They can move over their own player’s monsters, but cannot move over other player’s monsters.
    If two monsters finish on the same square, they are dealt with as follows:
    ●	If there’s a vampire and a werewolf, the werewolf is removed
    ●	If there’s a werewolf and a ghost, the ghost is removed
    ●	If there’s a ghost and a vampire, the vampire is removed
    ●	If there’s two of the same kind of monster, both are removed
    
    A player’s turn ends when they decide so, or if they have no monsters left to move.
    A round end once all players have had a turn.
    A player is eliminated once 10 of their monsters have been removed. A player wins if all other players have been eliminated.
    
    Give the code in html, css and javascript

--- script.js
+++ script.js
@@ -1,1 +1,80 @@
-alert("Hello world");
+const board = document.getElementById('board');
+let currentPlayer;
+let players;
+let gameOver = false;
+
+function startGame() {
+    currentPlayer = 0;
+    players = [
+        { name: 'Player 1', monstersLeft: 10, monsters: [] },
+        { name: 'Player 2', monstersLeft: 10, monsters: [] }
+    ];
+    gameOver = false;
+    renderBoard();
+}
+
+function renderBoard() {
+    board.innerHTML = '';
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.dataset.row = i;
+            square.dataset.col = j;
+            square.addEventListener('click', () => {
+                if (!gameOver) {
+                    handleSquareClick(square);
+                }
+            });
+            board.appendChild(square);
+        }
+    }
+}
+
+function handleSquareClick(square) {
+    const row = parseInt(square.dataset.row);
+    const col = parseInt(square.dataset.col);
+    if (players[currentPlayer].monstersLeft > 0) {
+        placeMonster(row, col);
+    } else {
+        moveMonster(row, col);
+    }
+}
+
+function placeMonster(row, col) {
+    const monsterTypes = ['vampire', 'werewolf', 'ghost'];
+    const randomMonster = monsterTypes[Math.floor(Math.random() * monsterTypes.length)];
+    const monster = { type: randomMonster, row, col };
+    players[currentPlayer].monsters.push(monster);
+    players[currentPlayer].monstersLeft--;
+    updateBoard();
+    switchPlayer();
+}
+
+function moveMonster(row, col) {
+    // Implement monster movement logic here
+}
+
+function updateBoard() {
+    board.querySelectorAll('.square').forEach(square => {
+        square.classList.remove('vampire', 'werewolf', 'ghost');
+    });
+    players.forEach(player => {
+        player.monsters.forEach(monster => {
+            const square = board.querySelector(`.square[data-row="${monster.row}"][data-col="${monster.col}"]`);
+            square.classList.add(monster.type);
+        });
+    });
+}
+
+function switchPlayer() {
+    currentPlayer = (currentPlayer + 1) % 2;
+    if (players[currentPlayer].monstersLeft === 0) {
+        endTurn();
+    }
+}
+
+function endTurn() {
+    // Implement logic to check for winner, end round, and eliminate players here
+    // Set gameOver to true when game ends
+}
