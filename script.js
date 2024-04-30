7b692c6db1e45ed7874b8476c8f300ace3dea112
Sam Weiss <sweiss@cct.ie>
2024-04-30 16:34:16 +0100

    Coppied generated code from below prompt
    it needs to have four players
    
    Each player can only play on their edge of the board
    
    Each player can only play one monster at a time
    
    After they play a monster, they can move any other monster they have placed

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ let gameOver = false;
 function startGame() {
     currentPlayer = 0;
     players = [
-        // Only two players, can only play 10 monsters
-        { name: 'Player 1', monstersLeft: 10, monsters: [] },
-        { name: 'Player 2', monstersLeft: 10, monsters: [] }
+        { name: 'Player 1', monstersLeft: 10, monsters: [], edge: 'top' },
+        { name: 'Player 2', monstersLeft: 10, monsters: [], edge: 'right' },
+        { name: 'Player 3', monstersLeft: 10, monsters: [], edge: 'bottom' },
+        { name: 'Player 4', monstersLeft: 10, monsters: [], edge: 'left' }
     ];
     gameOver = false;
     renderBoard();
@@ -22,64 +23,29 @@ function renderBoard() {
             square.classList.add('square');
             square.dataset.row = i;
             square.dataset.col = j;
-            square.addEventListener('click', () => {
-                if (!gameOver) {
-                    handleSquareClick(square);
-                }
-            });
             board.appendChild(square);
         }
     }
+    addEdgeClasses();
 }
 
-function handleSquareClick(square) {
-    const row = parseInt(square.dataset.row);
-    const col = parseInt(square.dataset.col);
-    // Can only place 10 monsters
-    if (players[currentPlayer].monstersLeft > 0) {
-        placeMonster(row, col);
-    } else {
-        moveMonster(row, col);
-    }
-}
-
-function placeMonster(row, col) {
-    const monsterTypes = ['vampire', 'werewolf', 'ghost'];
-    // We don't get to chose the monster
-    const randomMonster = monsterTypes[Math.floor(Math.random() * monsterTypes.length)];
-    const monster = { type: randomMonster, row, col };
-    players[currentPlayer].monsters.push(monster);
-    players[currentPlayer].monstersLeft--;
-    updateBoard();
-    switchPlayer();
-}
-
-function moveMonster(row, col) {
-    // Implement monster movement logic here
-}
-
-function updateBoard() {
+function addEdgeClasses() {
     board.querySelectorAll('.square').forEach(square => {
-        // no way to tell which colour is which monster
-        square.classList.remove('vampire', 'werewolf', 'ghost');
-    });
-    players.forEach(player => {
-        player.monsters.forEach(monster => {
-            const square = board.querySelector(`.square[data-row="${monster.row}"][data-col="${monster.col}"]`);
-            square.classList.add(monster.type);
-        });
+        const row = parseInt(square.dataset.row);
+        const col = parseInt(square.dataset.col);
+        if (row === 0) {
+            square.classList.add('player1');
+        }
+        if (col === 9) {
+            square.classList.add('player2');
+        }
+        if (row === 9) {
+            square.classList.add('player3');
+        }
+        if (col === 0) {
+            square.classList.add('player4');
+        }
     });
 }
 
-function switchPlayer() {
-    currentPlayer = (currentPlayer + 1) % 2;
-    // each player plays all their monsters, then the other player goes
-    if (players[currentPlayer].monstersLeft === 0) {
-        endTurn();
-    }
-}
-
-function endTurn() {
-    // Implement logic to check for winner, end round, and eliminate players here
-    // Set gameOver to true when game ends
-}
+// Other functions (placeMonster, moveMonster, updateBoard, switchPlayer, endTurn) remain the same
