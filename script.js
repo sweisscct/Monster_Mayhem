a2d10fa52139e5b582e460fb4b3b422f45fa9c49
Sam Weiss <sweiss@cct.ie>
2024-04-30 16:43:42 +0100

    Reintoduced previous version
    kept new startGame()
    
    added call to addEdgeClasses(); at the end of renderBoard()

--- script.js
+++ script.js
@@ -3,6 +3,77 @@ let currentPlayer;
 let players;
 let gameOver = false;
 
+function renderBoard() {
+    board.innerHTML = '';
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.dataset.row = i;
+            square.dataset.col = j;
+            square.addEventListener('click', () => {
+                if (!gameOver) {
+                    handleSquareClick(square);
+                }
+            });
+            board.appendChild(square);
+        }
+    }
+    addEdgeClasses();
+}
+
+function handleSquareClick(square) {
+    const row = parseInt(square.dataset.row);
+    const col = parseInt(square.dataset.col);
+    if (players[currentPlayer].monstersLeft > 0) {
+        placeMonster(row, col);
+    } else {
+        moveMonster(row, col);
+    }
+}
+
+function placeMonster(row, col) {
+    const monsterTypes = ['vampire', 'werewolf', 'ghost'];
+    const randomMonster = monsterTypes[Math.floor(Math.random() * monsterTypes.length)];
+    const monster = { type: randomMonster, row, col };
+    players[currentPlayer].monsters.push(monster);
+    players[currentPlayer].monstersLeft--;
+    updateBoard();
+    switchPlayer();
+}
+
+function moveMonster(row, col) {
+    // Implement monster movement logic here
+}
+
+function updateBoard() {
+    board.querySelectorAll('.square').forEach(square => {
+        square.classList.remove('vampire', 'werewolf', 'ghost');
+    });
+    players.forEach(player => {
+        player.monsters.forEach(monster => {
+            const square = board.querySelector(`.square[data-row="${monster.row}"][data-col="${monster.col}"]`);
+            square.classList.add(monster.type);
+        });
+    });
+}
+
+function switchPlayer() {
+    currentPlayer = (currentPlayer + 1) % 2;
+    if (players[currentPlayer].monstersLeft === 0) {
+        endTurn();
+    }
+}
+
+function endTurn() {
+    // Implement logic to check for winner, end round, and eliminate players here
+    // Set gameOver to true when game ends
+}
+
+
+
+/************************************************ */
+
 function startGame() {
     currentPlayer = 0;
     players = [
@@ -15,19 +86,6 @@ function startGame() {
     renderBoard();
 }
 
-function renderBoard() {
-    board.innerHTML = '';
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            const square = document.createElement('div');
-            square.classList.add('square');
-            square.dataset.row = i;
-            square.dataset.col = j;
-            board.appendChild(square);
-        }
-    }
-    addEdgeClasses();
-}
 
 function addEdgeClasses() {
     board.querySelectorAll('.square').forEach(square => {
